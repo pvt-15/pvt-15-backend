935c7c415e8020b4f756b941f1c3420ccd439700
gustafardbo <gustaf.ardbo@gmail.com>
2026-04-15 10:49:31 +0200

    Deletion of testcontainer

--- src/test/java/com/example/accessingdatamysql/TestAccessingDataMysqlApplication.java
+++ src/test/java/com/example/accessingdatamysql/TestAccessingDataMysqlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestAccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(AccessingDataMysqlApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(AccessingDataMysqlApplication::main).run(args);
 	}
 
 }
