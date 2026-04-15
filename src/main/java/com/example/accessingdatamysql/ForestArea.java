9865826a269babc907e673f12dbbaeee12627a77
gustafardbo <gustaf.ardbo@gmail.com>
2026-04-15 10:22:48 +0200

    ForestArea implementation

---
+++ src/main/java/com/example/accessingdatamysql/ForestArea.java
@@ -0,0 +1,85 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class ForestArea {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+    private String description;
+    private String location;
+    private int latitude;
+    private int longitude;
+    private int difficulty;
+    private int ageRecommendation;
+
+    public ForestArea(String name,
+                      String description,
+                      String location,
+                      int latitude,
+                      int longitude,
+                      int difficulty,
+                      int ageRecommendation){
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.difficulty = difficulty;
+        this.ageRecommendation = ageRecommendation;
+    }
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public int getAgeRecommendation() {
+        return ageRecommendation;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setAgeRecommendation(int ageRecommendation) {
+        this.ageRecommendation = ageRecommendation;
+    }
+}
--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -15,6 +15,9 @@ public class User {
 
     private String name;
     private String email;
+    private String passwordHash;
+    private int totalPoints;
+    private Level level;
 
     public User() {
     }
