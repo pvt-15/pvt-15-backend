c0bba508ad43ddc240042a520e3fc12976b0f277
gustafardbo <gustaf.ardbo@gmail.com>
2026-04-15 11:19:30 +0200

    Implemented challenge

--- src/main/java/com/example/accessingdatamysql/Challenge.java
+++ src/main/java/com/example/accessingdatamysql/Challenge.java
@@ -13,5 +13,61 @@ public class Challenge {
     private String title;
     private String description;
     private int points;
-    private int
+    private AgeGroup ageGroup;
+    private ChallengeType challengeType;
+
+    // Constructors
+    public Challenge(String title,
+                     String description,
+                     int points,
+                     AgeGroup ageGroup,
+                     ChallengeType challengeType){
+        this.title = title;
+        this.description = description;
+        this.points = points;
+        this.ageGroup = ageGroup;
+        this.challengeType = challengeType;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public AgeGroup getAgeGroup() {
+        return ageGroup;
+    }
+
+    public ChallengeType getChallengeType() {
+        return challengeType;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void setAgeGroup(AgeGroup ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
+    public void setChallengeType(ChallengeType challengeType) {
+        this.challengeType = challengeType;
+    }
 }
