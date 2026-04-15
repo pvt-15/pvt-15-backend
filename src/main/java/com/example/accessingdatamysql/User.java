bfd05a07859e320b572cb189baee3ea38ccf8e5a
gustafardbo <gustaf.ardbo@gmail.com>
2026-04-15 11:59:43 +0200

    Changed from jakarta to javax

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -1,10 +1,9 @@
 package com.example.accessingdatamysql;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
@@ -19,7 +18,7 @@ public class User {
     public User() {
     }
 
-    public User(String name, String email){
+    public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
@@ -28,20 +27,20 @@ public class User {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setId(Integer id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
