3d0795546a731220aeb8c231c74741c1d4d9e77f
gustafardbo <gustaf.ardbo@gmail.com>
2026-04-22 16:42:10 +0200

    UserRepository documentation

--- src/main/java/com/example/accessingdatamysql/user/repository/UserRepository.java
+++ src/main/java/com/example/accessingdatamysql/user/repository/UserRepository.java
@@ -6,13 +6,42 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for database access to {@code User}-entities
+ *
+ * <p>Interface extends {@link CrudRepository}, and therefore enables
+ * basic CRUD-support for users with {@code Integer} as primary key.</p>
+ *
+ * <p>Gives specialised search methods which is used in authentication,
+ * e.g. search by email and provider. </p>
+ */
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+    /**
+     * Fetches a user based on email.
+     *
+     * @param email email to search for
+     * @return a {@code Optional}, containing a user if a match is found,
+     *         otherwise an empty {@code Optional}
+     */
     Optional<User> findByEmail(String email);
 
+    /**
+     * Checks if there exists a user with the given email.
+     *
+     * @param email email to search for
+     * @return {@code true} if a user with this email is found,
+     *         otherwise {@code false}
+     */
     boolean existsByEmail(String email);
 
-    Optional<User> findByEmailAndProvider(String email, Provider provider);
-
+    /**
+     * Fetches a user based on provider and provider-specific user-ID
+     *
+     * @param provider the external provider the account belongs to
+     * @param providerUserId the providers unique ID for the user
+     * @return a {@code Optional}, containing a user if a match is found,
+     *         otherwise an empty {@code Optional}
+     */
     Optional<User> findByProviderAndProviderUserId(Provider provider, String providerUserId);
 }
