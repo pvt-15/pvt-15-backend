1019d5344db2f1e3edf4aef52de8a193f76b99b1
Gustaf Ardbo <gustaf.ardbo@gmail.com>
2026-04-27 09:21:07 +0200

    Added /

--- src/main/java/com/example/accessingdatamysql/picture/service/DiscoveryService.java
+++ src/main/java/com/example/accessingdatamysql/picture/service/DiscoveryService.java
@@ -1,7 +1,26 @@
 package com.example.accessingdatamysql.picture.service;
 
+import com.example.accessingdatamysql.model.User;
+import com.example.accessingdatamysql.model.enums.PictureCategory;
+import com.example.accessingdatamysql.picture.entity.UserDiscovery;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiscoveryService {
+
+    private static final int NEW_UNIQUE_POINTS = 5;
+    private static final int MILESTONE_BONUS = 20;
+
+    private final UserDiscoveryRepository userDiscoveryRepository;
+
+    public DiscoveryService(UserDiscoveryRepository userDiscoveryRepository){
+        this.userDiscoveryRepository = userDiscoveryRepository;
+    }
+
+    public int awardCollectionPoints(User user, PictureCategory pictureCategory, String label){
+        String normalizedLabel = normalizeLabel(label);
+
+        boolean alreadyExists = userDiscoveryRepository.existsByUserAndCategoryAndNormalizedLabel
+    }
+
 }
