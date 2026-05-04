313889e72c25d5fa6b3b44be265f90fc9e0febe4
gustafardbo <gustaf.ardbo@gmail.com>
2026-05-04 16:00:55 +0200

    Debugging logic

--- src/main/java/com/example/accessingdatamysql/storage/config/GoogleCloudStorageConfig.java
+++ src/main/java/com/example/accessingdatamysql/storage/config/GoogleCloudStorageConfig.java
@@ -15,6 +15,10 @@ public class GoogleCloudStorageConfig {
     @Bean
     public Storage storage(StorageProperties storageProperties) {
         try {
+            if (storageProperties.getCredentialsBase64() == null || storageProperties.getCredentialsBase64().isBlank()) {
+                throw new IllegalStateException("gcs.credentials.base64 is missing");
+            }
+
             byte[] decoded = Base64.getDecoder().decode(storageProperties.getCredentialsBase64());
 
             GoogleCredentials credentials = GoogleCredentials.fromStream(
