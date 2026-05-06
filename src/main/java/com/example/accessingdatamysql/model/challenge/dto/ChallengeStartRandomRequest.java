58dba459acaf9389d16c20fee6d4a8667e7c7c70
gustafardbo <gustaf.ardbo@gmail.com>
2026-05-06 10:33:24 +0200

    Adding new endpoint for frontend by challengecategory

--- src/main/java/com/example/accessingdatamysql/model/challenge/dto/ChallengeStartRandomRequest.java
+++ src/main/java/com/example/accessingdatamysql/model/challenge/dto/ChallengeStartRandomRequest.java
@@ -4,6 +4,7 @@ public class ChallengeStartRandomRequest {
 
     private String challengeDifficulty;
     private String challengeType;
+    private String challengeCategory;
 
     public ChallengeStartRandomRequest() {
     }
@@ -22,5 +23,13 @@ public class ChallengeStartRandomRequest {
 
     public void setChallengeType(String challengeType) {
         this.challengeType = challengeType;
+    }
+
+    public String getChallengeCategory() {
+        return challengeCategory;
+    }
+
+    public void setChallengeCategory(String challengeCategory) {
+        this.challengeCategory = challengeCategory;
     }
 }
